5a5ef47b941760f14a48cd3a812408bb683e7634
Jérémie Allard <jerall24@gmail.com>
2018-11-01 12:08:01 -0400

    added code to create binary data frame of courses, write out to excel, and created the excel sheet with the information.

--- CourseSetsLibrary.py
+++ CourseSetsLibrary.py
@@ -1,5 +1,7 @@
 import CourseSetsClass as csc
 import pandas as pd
+from pandas import ExcelWriter
+import numpy as np
 
 excel_file_name = "Initial Data Pull.xlsx"
 sheet_in_excel_file = 'Export Worksheet'
@@ -18,5 +20,11 @@ for student in range(0,len(student_paths)):
    binary_course_list.append(student_paths[student].returnCourses())
   # print(student_paths[student].returnPennID(), student_paths[student].returnCourses())
 aggregated_binary_list = [sum(x) for x in zip(*binary_course_list)]
-print(aggregated_binary_list)
-#print(list_of_courses[65])
+# print(aggregated_binary_list)
+# print(list_of_courses[65])
+course_array = np.array(binary_course_list)
+course_df = pd.DataFrame(data=course_array,index=list_of_penn_ids,columns=list_of_courses)
+writer = pd.ExcelWriter('Student Course Matrix.xlsx')
+course_df.to_excel(writer,'Sheet 1',index=True)
+#create a  file that will write to an excel sheet the binary listing of the classes with classes as headers
+#this is mostly be for reading and efficiency
